7df25513527e6f59e035594fd181f28ab22e4337
Ericsson ISHAKA <123806773+ericsson048@users.noreply.github.com>
2024-05-01 08:47:35 +0000

    Initial commit

---
+++ src/componets/Header.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+// import HowToRegIcon from "@mui/icons-material/HowToReg";
+
+function Header() {
+  return (
+    <header className="Header">
+      <div>
+        <h1>Stream</h1>
+        <nav>
+          <ul>
+            <li>Accueil</li>
+            <li>Stream</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
+      </div>
+      <div>
+        <h2>S'identifier</h2>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
